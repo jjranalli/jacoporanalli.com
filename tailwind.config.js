3972a3a11d0f3b9bb6469bd1c1774a6be32d16af
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2021-04-22 14:00:28 +0200

    upgrade to nightwind 1.1.11

--- tailwind.config.js
+++ tailwind.config.js
@@ -48,9 +48,5 @@ module.exports = {
     },
   },
 
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/aspect-ratio"),
-    require("nightwind"),
-  ],
+  plugins: [require("@tailwindcss/typography"), require("nightwind")],
 }
